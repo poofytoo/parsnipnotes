96427b36f813a94b8e1aa5b728fbebe0825121a4
poofytoo <victorhung92@gmail.com>
2013-03-04 02:56:05 -0500

    CSS Shark started

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express', packName: req.params.packName });
+  res.render('main', { title: 'Express', packName: req.params.packName });
 };
